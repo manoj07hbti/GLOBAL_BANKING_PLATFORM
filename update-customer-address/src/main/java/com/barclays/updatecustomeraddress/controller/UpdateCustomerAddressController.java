5d1715a63b9f481405079adc6ee3874df8f3060a
60manojkumar@gmail.com <Motivated@2019>
2022-01-18 13:05:38 +0530

    updated http method

--- update-customer-address/src/main/java/com/barclays/updatecustomeraddress/controller/UpdateCustomerAddressController.java
+++ update-customer-address/src/main/java/com/barclays/updatecustomeraddress/controller/UpdateCustomerAddressController.java
@@ -5,6 +5,7 @@ import com.barclays.updatecustomeraddress.service.UpdateAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class UpdateCustomerAddressController {
     @Autowired
     UpdateAddressService updateAddressService;
 
-    @RequestMapping("/update_address")
+    @RequestMapping(value = "/update_address",method = RequestMethod.PUT)
     public String updateAddress(@RequestBody CustomerAddress customerAddress){
       return updateAddressService.updateAddress(customerAddress);
     }
